6559d33c9fe3b62e8d337c20a57a9f998df54e3a
paulk <paulk@asert.com.au>
2017-05-31 07:58:46 +1000

    Update to latest versions

--- JBehave/src/NewPost.groovy
+++ JBehave/src/NewPost.groovy
@@ -1,4 +1,6 @@
-//@Grab('org.jbehave:jbehave-core:3.5.1')
+//@Grab('org.jbehave:jbehave-core:4.1.1')
+//@GrabConfig(systemClassLoader = true)
+//@Grab('xml-apis:xml-apis:1.4.01')
 import org.jbehave.core.junit.JUnitStory
 import org.jbehave.core.configuration.Configuration
 import org.jbehave.core.configuration.MostUsefulConfiguration
@@ -10,7 +12,7 @@ import org.jbehave.core.steps.InstanceStepsFactory
 class NewPost extends JUnitStory {
     Configuration configuration() {
         return new MostUsefulConfiguration()
-            .useStoryLoader(new LoadFromRelativeFile(new File('JBehave/src').toURL()))
+            .useStoryLoader(new LoadFromRelativeFile(new File('src').toURL()))
             .useStoryReporterBuilder(new StoryReporterBuilder().withDefaultFormats().withFormats(Format.CONSOLE))
     }
  
