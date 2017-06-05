f1b7824f8979714b8c4d7ca4745c165c97bfdd4d
paulk <paulk@asert.com.au>
2017-06-05 23:29:11 +1000

    upgrade Selenium WebDriver to 3.4.0

--- WebDriver/src/main/groovy/TestSimpBlogWebDriver.groovy
+++ WebDriver/src/main/groovy/TestSimpBlogWebDriver.groovy
@@ -1,10 +1,18 @@
-//@Grab('org.seleniumhq.selenium:selenium-htmlunit-driver:2.52.0')
-//@Grab('org.seleniumhq.selenium:selenium-support:2.52.0')
+//@Grab('org.seleniumhq.selenium:htmlunit-driver:2.27')
+//@Grab('org.seleniumhq.selenium:selenium-support:3.4.0')
 //@GrabExclude('xml-apis:xml-apis')
 import org.openqa.selenium.By
+//import org.openqa.selenium.chrome.ChromeDriver
+//import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
+//import org.openqa.selenium.ie.InternetExplorerDriver
 
+// uncomment one driver from below
+//def driver = new ChromeDriver()
+//def driver = new FirefoxDriver()
 def driver = new HtmlUnitDriver()
+//def driver = new InternetExplorerDriver()
+
 driver.get('http://localhost:8080/postForm')
 assert driver.title == 'Welcome to SimpBlog'
 
@@ -14,6 +22,8 @@ def select = driver.findElement(By.name('category'))
 select.findElements(By.tagName("option")).find{ it.text == 'School' }.click()
 driver.findElement(By.name('content')).sendKeys('Cowabunga dude!')
 driver.findElement(By.name('btnPost')).click()
+
+sleep 100
 
 assert driver.findElement(By.tagName("h1")).text.matches('Post.*: Bart was here.*')
 def h3headers = driver.findElements(By.tagName("h3"))
@@ -26,3 +36,5 @@ def row = driver.findElement(By.xpath("//table/tbody/tr"))
 def col = row.findElement(By.tagName("td"))
 def para = col.findElement(By.tagName("p"))
 assert para.text == 'Cowabunga dude!'
+
+driver.close()
--- WebDriver/src/main/groovy/TestSimpBlogWebDriverPages.groovy
+++ WebDriver/src/main/groovy/TestSimpBlogWebDriverPages.groovy
@@ -1,36 +1,50 @@
-//@Grab('org.seleniumhq.selenium:selenium-htmlunit-driver:2.52.0')
-//@Grab('org.seleniumhq.selenium:selenium-support:2.52.0')
+//@Grab('org.seleniumhq.selenium:htmlunit-driver:2.27')
+//@Grab('org.seleniumhq.selenium:selenium-support:3.4.0')
 //@GrabExclude('xml-apis:xml-apis')
 import org.openqa.selenium.By
+//import org.openqa.selenium.chrome.ChromeDriver
+//import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
+//import org.openqa.selenium.ie.InternetExplorerDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.PageFactory
 import org.openqa.selenium.WebDriver
 
+// uncomment one driver from below
+//def driver = new ChromeDriver()
+//def driver = new FirefoxDriver()
 def driver = new HtmlUnitDriver()
+//def driver = new InternetExplorerDriver()
+
 driver.get('http://localhost:8080/postForm')
 assert driver.title == 'Welcome to SimpBlog'
 
-def newBlogPage = PageFactory.initElements(driver, NewPostPage)
+def newBlogPage = new NewPostPage(driver)
 newBlogPage.title('Bart was here (WebDriver)')
 newBlogPage.author('Bart')
 newBlogPage.category('School')
 newBlogPage.content('Cowabunga dude!')
-newBlogPage.post() // could get this to return a ViewPostPage
+def viewBlogPage = newBlogPage.post() // could get this to return a ViewPostPage
 
-def viewBlogPage = PageFactory.initElements(driver, ViewPostPage)
 assert viewBlogPage.mainHeading() ==~ 'Post.*: Bart was here.*'
 assert viewBlogPage.categoryHeading() == 'Category: School'
 assert viewBlogPage.authorHeading() == 'Author: Bart'
 assert viewBlogPage.blogText() == 'Cowabunga dude!'
 
+driver.close()
+
 class NewPostPage {
+    private driver
+    NewPostPage(WebDriver driver) {
+        this.driver = driver
+        PageFactory.initElements(driver, this)
+    }
     private WebElement title, category, author, content, btnPost
     def title(title) { this.title.sendKeys(title) }
-    def category(category) { this.category.findElements(By.tagName("option")).find{ it.text == category }.setSelected() }
-    def author(author) { this.author.findElements(By.tagName("option")).find{ it.text == author }.setSelected() }
+    def category(category) { this.category.findElements(By.tagName("option")).find{ it.text == category }.click() }
+    def author(author) { this.author.findElements(By.tagName("option")).find{ it.text == author }.click() }
     def content(content) { this.content.sendKeys(content) }
-    def post() { btnPost.click() }
+    ViewPostPage post() { btnPost.click(); sleep 100; new ViewPostPage(driver) }
 }
 
 class ViewPostPage {
