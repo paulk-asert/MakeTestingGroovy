5cb5e84897fe1203327c8c1adc5827c6a2a95802
paulk <paulk@asert.com.au>
2017-06-16 01:18:17 +1000

    add Spock Genesis example

---
+++ PropertyBased/src/test/groovy/TestSimpBlogGenesis.groovy
@@ -0,0 +1,27 @@
+//@Grab('org.functionaljava:functionaljava-quickcheck:4.7')
+import spock.genesis.transform.Iterations
+import spock.lang.Specification
+import static spock.genesis.Gen.*
+
+class TestSimpBlogGenesis extends Specification {
+    static authors = ["Bart", "Homer", "Lisa", "Marge", "Maggie"]
+    static categories = ["Home", "Work", "Food", "Travel", "School"]
+
+    @Iterations(10)
+    def 'the blog should be posted'() {
+        given:
+        def tester = new BlogTesterBoolean('http://localhost:8080/postForm')
+
+        when:
+        tester.postBlog title: title, category: category, content: content, author: author
+
+        then:
+        tester.checkAll title, category, author, content
+
+        where:
+        author << any(authors)
+        category << integer(0..<categories.size()).map{ categories[it] }
+        title << string(40).map{ it + ' (genesis)' }
+        content << string(255)
+    }
+}
