fcba70f0f4fbb5c14d3086cac09540fc40e88100
Paul King <paulk@asert.com.au>
2018-05-06 11:02:11 +1000

    move Slim and WebTest to archive branch

--- Slim/src/test/groovy/FitNesseHsacSuiteRunnerTest.groovy
+++
@@ -1,8 +0,0 @@
-import fitnesse.junit.FitNesseRunner
-import org.junit.runner.RunWith
-import util.CustomHsacRunner
-
-@RunWith(CustomHsacRunner)
-@FitNesseRunner.Suite("FitNesse.SimpBlogHsacSuite.SimpBlogTest")
-class FitNesseHsacSuiteRunnerTest {}
-
